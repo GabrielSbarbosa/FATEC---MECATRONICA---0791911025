916c6242421272c0fc417db1af1c5a8fcf066999
GabrielSbarbosa <69904674+GabrielSbarbosa@users.noreply.github.com>
2020-11-18 13:05:42 -0300

    Create programa02.c

---
+++ Pasta02/programa02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+
+float A,B,X;
+
+printf(" Escreva o valor de B e A: ");
+scanf("%f%f",&B,&A);
+
+X = -B/A; 
+
+printf(" Valor de x é : %.2f \n ", X);
+ return 0;
+}
