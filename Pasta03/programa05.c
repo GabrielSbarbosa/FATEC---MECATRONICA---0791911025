bbb914ded22c526bfb8fc7bb66778355de6c987c
GabrielSbarbosa <69904674+GabrielSbarbosa@users.noreply.github.com>
2020-11-18 13:13:14 -0300

    Create programa05.c

---
+++ Pasta03/programa05.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main(void){
+int minhaVariavel;
+minhaVariavel = 10;
+printf("Valor da minhaVariavel: %i\n", minhaVariavel); // apresenta o conteudo
+printf("Valor da minhaVariavel: %i\n", &minhaVariavel); // apreseenta o endereço
+return 0;
+}
