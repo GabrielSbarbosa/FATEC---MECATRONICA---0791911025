2a56933333081366aa9a1a35aa021eb06de4fd45
GabrielSbarbosa <69904674+GabrielSbarbosa@users.noreply.github.com>
2020-11-18 11:14:43 -0300

    Create programa04.ino

---
+++ pasta01/programa04.ino
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+
+float numero_real;
+int numero_inteiro;
+char letra;
+
+printf("Informe um numero real: ");
+scanf("%f", &numero_real);
+printf("Informe um numero inteiro: ");
+scanf("%i", &numero_inteiro);
+printf("Informe uma letra: ");
+scanf(" %c", &letra);
+
+printf("Valor Real: %f \n", numero_real);
+printf("Valor inteiro: %d \n", numero_inteiro);
+printf("Letra: %c \n", letra);
+printf("Palmeiras não tem mundial! \n");
+
+ return 0;
+}
